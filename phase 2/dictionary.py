65cf41e7277797a41664918f071c00f58ba72cfe
Immu <immui1413@gmail.com>
2026-04-01 06:43:08 +0000

    a

---
+++ phase 2/dictionary.py
@@ -0,0 +1,18 @@
+#basic structure of dictionary
+profile = {
+    "name" : "imran",
+    "age" : 19,
+    "city" : "chikaballapur"
+}
+print(profile)
+
+#accessing elements 
+print(profile["name"])
+
+#updatee lements 
+profile["name"] = "aliya"
+print("updated name",profile["name"])
+
+#adding elements :
+profile["frns"] = 3
+print("added frds",profile["frns"])

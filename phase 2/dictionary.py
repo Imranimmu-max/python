48effdb84da058cb75ac49d5192119c827b5ad98
Immu <immui1413@gmail.com>
2026-04-04 14:24:26 +0000

    now doing on pc

--- phase 2/dictionary.py
+++ phase 2/dictionary.py
@@ -7,5 +7,16 @@ print(profile)
 
 for i in profile:
     print(i,":",profile[i])
+    print(profile.keys())
 
 print(profile.keys())
+print(profile.values())
+print(profile.items())
+
+std = {}
+
+std["name"] = input("enter ur name : ")
+std["age"]  = int(input("enter u r age "))
+std["city"] = input("enter u r city")
+
+print(std)
